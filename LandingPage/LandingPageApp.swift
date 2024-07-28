37aa58c7df84b079ac5ae16a4942d9d91919d544
Sudipta Chowdhury <sudipta.chowdhury@welldev.io>
2024-07-28 16:18:34 +0600

    Initial Commit

---
+++ LandingPage/LandingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandingPageApp.swift
+//  LandingPage
+//
+//  Created by Sudipta Chowdhury on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct LandingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
